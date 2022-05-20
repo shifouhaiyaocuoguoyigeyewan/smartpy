542faf9fa2ce75555ffb81b1f9887c460bb5c8e3
shifouhaiyaocuoguoyigeyewan <2487242076@qq.com>
2022-05-20 19:05:34 +0800

    SmartPy

--- part4/ZooNFT.py
+++
@@ -1,5 +0,0 @@
-import smartpy as sp
-
-class ZooNFT_FA2(sp.Contract):
-    def __init__(self):
-        pass
---
+++ part4/ZooNFT_FA2.py
@@ -0,0 +1,5 @@
+import smartpy as sp
+
+class ZooNFT_FA2(sp.Contract):
+    def __init__(self):
+        pass
